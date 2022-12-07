877fe28e86979fe3a79d0ccbdb9e10e469de6faf
Jun Yu <maddreamer@nate.com>
2022-12-07 15:18:00 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Akka.Actor;
+
+namespace WinTail
+{
+  #region Program
+
+  internal class Program
+  {
+    public static ActorSystem MyActorSystem;
+
+    private static void Main(string[] args)
+    {
+      // initialize MyActorSystem
+      // YOU NEED TO FILL IN HERE
+      MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+      PrintInstructions();
+
+      // time to make your first actors!
+      //YOU NEED TO FILL IN HERE
+      // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
+      var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+      // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+      var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
+
+      // tell console reader to begin
+      //YOU NEED TO FILL IN HERE
+      consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+      // blocks the main thread from exiting until the actor system is shut down
+      MyActorSystem.WhenTerminated.Wait();
+    }
+
+    private static void PrintInstructions()
+    {
+      Console.WriteLine("Write whatever you want into the console!");
+      Console.Write("Some lines will appear as");
+      Console.ForegroundColor = ConsoleColor.DarkRed;
+      Console.Write(" red ");
+      Console.ResetColor();
+      Console.Write(" and others will appear as");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.Write(" green! ");
+      Console.ResetColor();
+      Console.WriteLine();
+      Console.WriteLine();
+      Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+    }
+  }
+
+  #endregion Program
+}
