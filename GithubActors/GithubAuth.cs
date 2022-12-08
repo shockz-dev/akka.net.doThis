b2c50670e259a4871e2dd2cbbc70685e0fafeb04
유치준 수석 <nerp1.cjyoo@halla.com>
2022-12-08 18:00:12 +0900

    ready to develope unit 3

---
+++ GithubActors/GithubAuth.cs
@@ -0,0 +1,22 @@
+namespace GithubActors
+{
+  public partial class GithubAuth : Form
+  {
+    public GithubAuth()
+    {
+      InitializeComponent();
+    }
+
+    private void GithubAuth_Load(object sender, EventArgs e)
+    {
+    }
+
+    private void linkGhLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+    }
+
+    private void btnAuthenticate_Click(object sender, EventArgs e)
+    {
+    }
+  }
+}
